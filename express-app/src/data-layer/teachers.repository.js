2b1a4b0c38a4231d9b74184ec5be4ba5350cc100
Nau077 <antbobrikov@yandex.ru>
2021-11-03 17:09:51 +0300

    fix with teachers routes

--- express-app/src/data-layer/teachers.repository.js
+++ express-app/src/data-layer/teachers.repository.js
@@ -1,11 +1,8 @@
 const knex = require("../../config/knex.config");
-const ApiError = require("../utils/ErrorHelper");
-
 const TEACHERS_TABLE = "teachers";
 
-module.exports = class StudentRepository {
+module.exports = class TeachersRepository {
   async getTeachers() {
-
     try {
       const teachers = await knex(TEACHERS_TABLE)
         .leftOuterJoin("subjects", "teachers.subject_id", "=", "subjects.id")
@@ -17,21 +14,16 @@ module.exports = class StudentRepository {
           "subjects.name as subject_name"
         );
 
-      if (!teachers.length) throw "Нет данных"
+      if (!teachers.length) throw "Нет данных";
 
       return teachers;
-    } catch (e) {
-      throw e;
+    } catch (error) {
+      throw error;
     }
   }
 
   async getOneTeacher(id) {
     try {
-      //   SELECT teachers.name, subjects.name
-      //   FROM teachers
-      //   INNER JOIN subjects
-      //   ON teachers.subject_id = subjects.id;
-
       const teacher = await knex(TEACHERS_TABLE)
         .innerJoin("subjects", "teachers.subject_id", "subjects.id")
         .select(
@@ -43,82 +35,81 @@ module.exports = class StudentRepository {
         )
         .where({ "teachers.id": id });
 
-        if (!teacher[0]) throw "Нет данных"
-
-        return teacher[0];
-    } catch (e) {
-      throw e;
-    }
-  }
-
-  async updateOneTeacher(id, fields) {
-   const trx = await knex.transaction();
-
-   try {
-     const result = await knex(TEACHERS_TABLE)
-       .transacting(trx)
-       .join()
-       .where({id})
-       .update({...fields, ...{updated_at: new Date()} })
-       .returning("*")
-
-     await trx.commit();
-
-     const updatedTeacher = await knex(TEACHERS_TABLE)
-     .innerJoin("subjects", "teachers.subject_id", "subjects.id")
-     .select(
-       "teachers.name as teacher_name",
-       "teachers.id",
-       "teachers.is_union_member",
-       "teachers.work_experience",
-       "subjects.name as subject_name"
-     )
-     .where({ "teachers.id": result[0].id });
-
-     if (!teacher[0]) throw "Нет данных"
-
-     return updatedTeacher[0];
-   } catch (e) {
-     trx.rollback();
-     throw e;
-   }
-  }
-
-  async removeOneTeacher(id) {
-    try {
-    const result = await knex(TEACHERS_TABLE)
-        .del()
-        .where({
-          id
-        })
-        .returning("id")
-
-    if (!result[0]) throw `Нет записи, найденных при удалении по соотв. параметру: ${id}`
-
-    return {
-        id: result[0]
-        }; 
+      if (!teacher[0]) throw "Нет данных";
+      return teacher[0];
     } catch (error) {
       throw error;
     }
   }
 
   async addTeacher(fields) {
-    const trx = await knex.transaction({isolation: 'repeatable read'});
+        const trx = await knex.transaction({isolation: 'repeatable read'});
     
-    try {
-      const result = await knex(TEACHERS_TABLE)
-        .transacting(trx)
-        .insert(fields)
-        .returning("id");
-      await trx.commit();
-     
-    } catch (e) {
-      trx.rollback();
-      let error = new ApiError("422", "Ошибка атомарности: " + e);
-      throw error;
+        try {
+           const result = await knex(TEACHERS_TABLE)
+            .transacting(trx)
+            .insert(fields)
+            .returning("id");
+           
+           await trx.commit();
+           
+           return result;
+        } catch (error) {
+            await trx.rollback();
+            throw "Ошибка атомарности " + error 
+        }
+    };
+
+    async removeOneTeacher(id) {
+        try {
+            const result = await knex(TEACHERS_TABLE)
+                .del()
+                .where({
+                    id
+                })
+                .returning("id");
+
+           if (!result[0]) throw `Нет записи, найденных при удалении по соотв. параметру: ${id}`;
+
+           return {
+               id : result[0]
+           };
+        } catch (error) {
+            throw error;
+        }
     }
-  }
-};
 
+    async updateOneTeacher(id, fields) {
+        const trx = await knex.transaction();
 
+        try {
+            const  result = await knex(TEACHERS_TABLE)
+                .transacting(trx)
+                .where({id})
+                .update({...fields, ...{updated_at: new Date()}})
+                .returning("*")
+                
+            await trx.commit();
+
+            const updatedTeacher = await knex(TEACHERS_TABLE)
+                .innerJoin("subjects", "teachers.subject_id", "subjects.id")
+                .select(
+                    "teachers.name as teacher_name",
+                    "teachers.id",
+                    "teachers.is_union_member",
+                    "teachers.work_experience",
+                    "subjects.name as subject_name"
+                )
+                .where({"teachers.id": result[0].id})
+            
+            
+            
+            if (!updatedTeacher[0]) throw "Нет данных";
+
+            return updatedTeacher[0];
+        } catch (error) {
+            trx.rollback();
+            throw error;
+        }
+    }
+}
--- express-app/src/domain-layer/use-cases/teachers.use-case.js
+++ express-app/src/domain-layer/use-cases/teachers.use-case.js
@@ -1,69 +1,37 @@
-// teachers - insert - сделать метод checkStringFieldsInsert в репозитории
-// внутри use case
-// проверить delete
-
 const TeacherRepository = require("../../data-layer/teachers.repository");
-const ApiError = require("../../utils/ErrorHelper");
-const Teacher = require("../entities/teacher.entity");
+const Teacher = require("../entities/teacher.entitiy");
 
 module.exports = class TeacherUseCase {
-    mapFields = {
-        id: "id",
-        name: "name",
-        work_experience: "work_experience",
-        phone_number: "phone_number",
-        subject_id: "subject_id",
-        is_union_member: "is_union_member"
-    };
+
+  mapFields = {
+    id: "id",
+    name: "name",
+    work_experience: "work_experience",
+    phone_number: "phone_number",
+    subject_id: "subject_id",
+    is_union_member: "is_union_member"
+  }
 
   async getTeachers() {
     const teacherRepository = new TeacherRepository();
+
     try {
       const teachersDb = await teacherRepository.getTeachers();
-      const teachers = teachersDb.map(teacherDb => new Teacher(teacherDb))
+      const teachers = teachersDb.map((teacherDb) => new Teacher(teacherDb));
 
-      return teachers
+      return teachers;
     } catch (error) {
       throw error;
     }
   }
 
   async getOneTeacher(id) {
+    const teacherRepository = new TeacherRepository();
+
     try {
       const teacherDb = await teacherRepository.getOneTeacher(id);
       const teacher = new Teacher(teacherDb);
-      
-      return teacher;
-    } catch (error) {
-      throw error;
-    }
-  }
 
-  async updateTeacher(data) {
-    const teacherRepository = new TeacherRepository();
-
-    if (!data.fields && !Array.isArray(data.fields) && !data.fields.length) {
-      const error = new ApiError("422", "Нет данных для обновления");
-      throw error;
-    }
-    const elWithId = data.fields.find(el => el.id)
-
-    if (!elWithId) {
-      const error = new ApiError("422", "Нет id для обновления");
-      throw error;
-    }
-    const fields = data.fields;
-    const isStringChecked = this.checkStringFields(fields);
-
-    if (!isStringChecked) {
-      const error = new ApiError("422", "Неверные поля переданы");
-      throw error;
-    }
-
-    try {
-      const updatedTeacher = await teacherRepository.updateOneTeacher(elWithId.id, this.reducefields(fields));
-      const teacher = new Teacher(updatedTeacher);
-      
       return teacher;
     } catch (error) {
       throw error;
@@ -74,68 +42,100 @@ module.exports = class TeacherUseCase {
     const teacherRepository = new TeacherRepository();
 
     if (!data.fields && !Array.isArray(data.fields) && !data.fields.length) {
-      const error = new ApiError("422", "Нет данных для обновления");
-      throw error;
+        throw "Нет данных для вставки";
     }
+
     const fields = data.fields;
     const isStringChecked = this.checkStringFieldsInsert(fields);
+
     if (!isStringChecked) {
-      let error = new ApiError("422", "Неверные поля переданы");
-      throw error;
+        throw "Неверные поля переданы"
     }
 
     try {
-      const teacher = await teacherRepository.addTeacher(this.reducefields(fields));
-      return teacher;
-    } catch (error) {
-      throw error;
-    }
-  }
+        const teacher = await teacherRepository.addTeacher(this.reduceFields(fields));
 
-  async removeTeacher(id) {
-    try {
-    const teacherRepository = new TeacherRepository();
-    const result = await teacherRepository.removeOneTeacher(id);
-
-    return result;
+        return teacher
     } catch (error) {
         throw error;
     }
   }
 
-  checkStringFields(fields) {
-    const IS_VALID = true;
-    const fieldsNormalized = fields
-            .map(el => Object.entries(el)[0])
-    
-    fieldsNormalized.some(([key, _]) => {
-      if (!this.mapFields[key]) {
-        return !IS_VALID;
+  async removeTeacher(id) {
+      try {
+        const teacherRepository = new TeacherRepository();
+        const result = await teacherRepository.removeOneTeacher(id);
+
+        return result;
+      } catch (error) {
+          throw error;
       }
-    });
-  
-    return IS_VALID;
   }
 
-  reducefields(fields) {
-    return fields.reduce((acc, el) => {
-      const entriesEl = Object.entries(el);
-      acc[entriesEl[0][0]] = entriesEl[0][1];
-      return acc;
-    }, {});
+  async updateTeacher(data) {
+    const teacherRepository = new TeacherRepository();
+
+    if (!data.fields && !Array.isArray(data.fields) && !data.fields.length) {
+        throw "Нет данных для обновления";
+    }
+
+    const elWithId = data.fields.find(el => el.id)
+
+    if (!elWithId) {
+        throw "Нет id для обновления"
+    };
+
+    const fields = data.fields;
+    const isStringChecked  = this.checkStringFields(fields);
+
+    if (!isStringChecked) {
+        throw "Неверные поля переданы"
+    };
+
+    try {
+        const updatedTeacher = await teacherRepository.updateOneTeacher(elWithId.id, this.reduceFields(fields));
+ 
+        const teacher = new Teacher(updatedTeacher);
+        return teacher;
+    } catch (error) {
+        throw error;
+    }
   }
 
   checkStringFieldsInsert(fields) {
-    const keys = fields.map(val => {
-      return Object.entries(val)[0][0];
+      const keys = fields.map(val => {
+          return Object.entries(val)[0][0];
+      });
+
+      const IS_VALID = true;
+
+      for (let i = 0; i < keys.length; i++) {
+          if (!(keys[i]) in this.mapFields) return !IS_VALID;
+      }
+
+      return IS_VALID;
+  };
+
+  checkStringFields(fields) {
+    const IS_VALID = true;
+    const fieldsNormalized = fields
+        .map(el => Object.entries(el)[0][0])
+        
+    fieldsNormalized.some(([key, _]) => {
+        if (!this.mapFields[key]) {
+            return !IS_VALID
+        }
     });
 
-    const IS_VALID = true;
-
-    for (let i = 0; i < keys.length; i++) {
-      if (!(keys[i] in this.mapFields)) return !IS_VALID;
-    }
-  
     return IS_VALID;
-  }
+  };
+
+  reduceFields(fields) {
+      return fields.reduce((acc, el) => {
+          const entriesEl = Object.entries(el);
+          acc[entriesEl[0][0]] = entriesEl[0][1]
+
+          return acc;
+      }, {})
+  };
 };
